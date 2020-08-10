618f088471b2801b334f4ebd02ba092fc8bbcec5
Rakshit <panjwanirakshit04@gmail.com>
2020-08-10 17:37:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+var scores, roundScore, activePlayer, dice, gamePlaying;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener("click", function(){
+
+    if(gamePlaying){
+
+    var dice= 1+ Math.floor(Math.random()*6);
+
+    document.querySelector('.dice').style.display="block";
+
+    var diceDom= document.querySelector('.dice');
+
+    diceDom.src="dice-"+dice+".png"
+
+    if(dice!==1){
+        roundScore+=dice;
+        document.querySelector('#current-'+activePlayer).textContent=roundScore;
+    }
+    else{
+        // Mera manna hai ki ye kaam karna chahiye
+        // roundScore=0;
+        
+        nextPlayer()
+    }
+}
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+    if(gamePlaying){
+
+scores[activePlayer]+=roundScore;
+
+document.getElementById('score-'+activePlayer).textContent=scores[activePlayer];
+
+if(scores[activePlayer]>=100){
+    document.getElementById('name-'+activePlayer).textContent="Winner"
+    document.querySelector('.dice').style.display="none";
+
+    document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner')
+    document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active')
+    gamePlaying=false;
+}
+else{
+nextPlayer();
+}
+    }
+});
+
+function nextPlayer(){
+    activePlayer===0 ? activePlayer=1 : activePlayer=0;
+        roundScore=0;
+        document.getElementById('current-0').textContent="0";
+        document.getElementById('current-1').textContent="0";
+
+        document.querySelector('#current-0').textContent="0"
+        document.querySelector('#current-1').textContent="0"
+
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
+
+        document.querySelector('.dice').style.display="none"
+}
+
+document.querySelector('.btn-new').addEventListener('click',init)
+
+function init(){
+    gamePlaying=true;
+scores= [0,0];
+roundScore= 0;
+activePlayer= 0;
+
+
+
+// document.querySelector('#current-'+ activePlayer).textContent=dice;
+document.querySelector('.dice').style.display="none";
+
+document.getElementById('score-0').textContent="0"
+document.getElementById('score-1').textContent="0"
+document.getElementById('current-0').textContent="0"
+document.getElementById('current-1').textContent="0"
+document.getElementById('name-0').textContent="Player 1"
+document.getElementById('name-1').textContent="Player 2"
+document.querySelector('.player-0-panel').classList.remove('winner')
+document.querySelector('.player-1-panel').classList.remove('winner')
+document.querySelector('.player-0-panel').classList.remove('active')
+document.querySelector('.player-1-panel').classList.remove('active')
+document.querySelector('.player-0-panel').classList.add('active')
+}
